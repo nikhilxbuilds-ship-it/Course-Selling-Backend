475eb8069138ff1511925f1eb5c1cb1ddf016091
Nikhil Kumar <nikhilxbuilds@gmail.com>
2026-03-17 16:27:45 +0530

    Added admin routes

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const { courseRouter } = require('./routes/course')
 const { config } = require("dotenv");
 require("dotenv").config();
 
+// users routes
 app.use("/users", userRouter);
 app.use("/course", courseRouter)
 
-
+// admin routes
+app.use("/admin", adminRouter);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening at port ${process.env.PORT}`)
---
+++ routes/admin.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/singup", function(req, res) {
+
+})
+
+adminRouter.post("/login", function(req, res) {
+    
+})
+
+adminRouter.post("/course", function(req, res) {
+    
+})
+
+adminRouter.put("/course", function(req, res) {
+    
+})
+
+adminRouter.delete("/course", function(req, res) {
+    
+})
